3a2e2d18dc61dd2438dd6509b5be3581010cd678
FranJPS2000 <francisca.jps28@gmail.com>
2023-08-24 22:33:16 -0400

    Filtrar arreglo

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,21 @@
 iniciarSesion();
 
+const alimentos = [{tipo: 'Carbohidrato', nombre: 'Azucar'},
+                    {tipo: 'Carbohidrato', nombre: 'Manjar'},
+                    {tipo: 'Carbohidrato', nombre: 'Galletas'},
+                    {tipo: 'Carbohidrato', nombre: 'Pasteles'},
+                    {tipo: 'Carbohidrato', nombre: 'Avena'},
+                    {tipo: 'Proteina', nombre: 'Pechuga de pollo'},
+                    {tipo: 'Proteina', nombre: 'Pechuga de pavo'},
+                    {tipo: 'Proteina', nombre: 'Posta Rosada'},
+                    {tipo: 'Proteina', nombre: 'Soya'},
+                    {tipo: 'Proteina', nombre: 'Huevos'},
+                    {tipo: 'Grasa', nombre: 'Palta'},
+                    {tipo: 'Grasa', nombre: 'Aceite'},
+                    {tipo: 'Grasa', nombre: 'Mantequilla'},
+                    {tipo: 'Grasa', nombre: 'Semillas'},
+                    {tipo: 'Grasa', nombre: 'Manteca Animal'}];
+
 function iniciarSesion() {
     let nombre = prompt("Ingresar nombre de usuario");
 
@@ -9,4 +25,22 @@ function iniciarSesion() {
     }
 
     alert(`Bienvenido ${nombre}!`);
+}
+
+function filtrarAlimentos(){
+    let categoria = prompt("Ingresar tipo de alimento (Carbohidrato, Proteina, Grasa)");
+    while(categoria === undefined || categoria === null || categoria === ""){
+        alert("Debes ingresar una categoria valida");
+        nombre = prompt("Ingresar tipo de alimento (Carbohidrato, Proteina, Grasa)");
+    }
+
+    let alimentosFiltrados = alimentos.filter(alimento => alimento.tipo.toUpperCase() == categoria.toUpperCase());
+    
+    let alimentosAlert = "";
+
+    alimentosFiltrados.forEach(alimento => {
+        alimentosAlert = `${alimentosAlert} - ${alimento.nombre}`;
+    });
+
+    alert(`Alimentos encontrados: ${alimentosAlert}`);
 }
