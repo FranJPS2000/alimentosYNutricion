1c164f5b5e0299491f592a405a2b17a8d7d998a9
FranJPS2000 <francisca.jps28@gmail.com>
2023-09-07 23:12:01 -0300

    feature: add preEntrega3 updates

--- scripts/herramientas.js
+++ scripts/herramientas.js
@@ -1,58 +1,31 @@
 
-
-function promediarDatos(){
-    let datos = [];
-    let dato;
-
-    do{
-        dato = parseFloat(prompt("Ingresa un dato numerico (Ingresa 0 para terminar)"));
-        while(isNaN(dato)){
-            alert("Ingresa un dato valido");
-            dato = parseFloat(prompt("Ingresa un dato numerico (Ingresa 0 para terminar)"))
-        }
-
-        datos.push(dato);
-    }while(dato != 0);
-
-    datos.pop();
-
-    let promedio = 0;
-
-    datos.forEach(dato => {
-        promedio += dato;
-    });
-
-    promedio = promedio/datos.length;
-
-    alert(`El promedio de los datos es ${promedio}`);
-}
-
-function estimarAltura(sexoBiologico){
-    let alturaPadre;
-    let alturaMadre;
-
-    alturaPadre = parseFloat(prompt("Ingresa la altura de tu padre (en centimetros)"));
-    while(isNaN(alturaPadre)){
-        alert("Ingresa un dato valido");
-        alturaPadre = parseFloat(prompt("Ingresa la altura de tu padre (en centimetros)"));
-    }
-
-
-    alturaMadre = parseFloat(prompt("Ingresa la altura de tu madre (en centimetros)"));
-    while(isNaN(alturaMadre)){
-        alert("Ingresa un dato valido");
-        alturaMadre = parseFloat(prompt("Ingresa la altura de tu madre (en centimetros)"));
-    }
+function estimarAltura(){
+    let alturaPadre = document.getElementById("alturaPadre").value;
+    let alturaMadre = document.getElementById("alturaMadre").value;
+    let sexoBiologico = document.getElementById("hombre").checked;
 
     let altura;
-    if(sexoBiologico === "HOMBRE"){
-        altura = (alturaMadre+alturaPadre+13)/2;
+    if(sexoBiologico){
+        altura = (parseInt(alturaMadre)+parseInt(alturaPadre)+13)/2;
     }else{
-        altura = (alturaMadre+alturaPadre-13)/2;
+        altura = (parseInt(alturaMadre)+parseInt(alturaPadre)-13)/2;
     }
-    
 
-    alert(`Tu altura aproximada es entre ${altura-7.5}cm y ${altura+7.5}cm`);
+    if(isNaN(altura)){
+        Swal.fire({
+            title: 'Error en los datos',
+            text: `Ingresa datos validos`,
+            icon: 'error',
+            confirmButtonText: 'OK!'
+        });
+    }else{
+        Swal.fire({
+            title: 'Altura estimada',
+            text: `Tu altura aproximada es entre ${altura-7.5}cm y ${altura+7.5}cm`,
+            icon: 'success',
+            confirmButtonText: 'OK!'
+        });
+    }
 }
 
 function calcularIMC(){
@@ -67,29 +40,47 @@ function calcularIMC(){
         }
     }
 
+    altura = document.getElementById("altura");
+    peso = document.getElementById("peso");
 
-    let altura = parseFloat(prompt("Ingresa tu altura en metros"));
-    while(isNaN(altura) || altura == 1){
-        alert("Ingresa una altura valida");
-        altura = parseFloat(prompt("Ingresa tu altura en metros"));
-    }
+    const imc = (new IMC(altura.value, peso.value)).calcular();
 
-    let peso = parseFloat(prompt("Ingresa tu peso en kilogramos"));
-    while(isNaN(peso)){
-        alert("Ingresa un peso valido");
-        peso = parseFloat(prompt("Ingresa tu peso en kilogramos"));
-    }
-
-    const imc = (new IMC(altura, peso)).calcular();
-
-
-    if(imc < 18.5){
-        alert(`Tu imc es: ${imc.toFixed(1)}. Por lo que estas bajo de peso`)
-    }else if(imc <  25){
-        alert(`Tu imc es: ${imc.toFixed(1)}. Por lo que tu peso es ideal`)
-    }else if(imc < 30){
-        alert(`Tu imc es: ${imc.toFixed(1)}. Por lo que tienes sobrepeso`)
+    if(!isNaN(imc)){
+        if(imc < 18.5){
+            Swal.fire({
+                title: 'Estas bajo peso!',
+                text: `Tu imc es: ${imc.toFixed(1)}. Por lo que estas bajo de peso`,
+                icon: 'warning',
+                confirmButtonText: 'Entiendo'
+            });
+        }else if(imc <  25){
+            Swal.fire({
+                title: 'Estas sano!',
+                text: `Tu imc es: ${imc.toFixed(1)}. Por lo que tu peso es ideal`,
+                icon: 'success',
+                confirmButtonText: 'Entiendo'
+            });
+        }else if(imc < 30){
+            Swal.fire({
+                title: 'Tienes sobrepeso!',
+                text: `Tu imc es: ${imc.toFixed(1)}. Por lo que tienes sobrepeso`,
+                icon: 'warning',
+                confirmButtonText: 'Entiendo'
+            });
+        }else{
+            Swal.fire({
+                title: 'Tienes obesidad!',
+                text: `Tu imc es: ${imc.toFixed(1)}. Por lo que tienes obesidad`,
+                icon: 'error',
+                confirmButtonText: 'Entiendo'
+            });
+        }
     }else{
-        alert(`Tu imc es: ${imc.toFixed(1)}. Por lo que tienes obesidad`)
+        Swal.fire({
+            title: 'Error en los datos',
+            text: `Ingresa datos validos`,
+            icon: 'error',
+            confirmButtonText: 'OK!'
+        });
     }
 }
--- scripts/index.js
+++ scripts/index.js
@@ -1,46 +1,188 @@
-iniciarSesion();
+var listaAlimentos = [];
+var buscador = document.getElementById("buscador");
+actualizarLista();
 
-const alimentos = [{tipo: 'Carbohidrato', nombre: 'Azucar'},
-                    {tipo: 'Carbohidrato', nombre: 'Manjar'},
-                    {tipo: 'Carbohidrato', nombre: 'Galletas'},
-                    {tipo: 'Carbohidrato', nombre: 'Pasteles'},
-                    {tipo: 'Carbohidrato', nombre: 'Avena'},
-                    {tipo: 'Proteina', nombre: 'Pechuga de pollo'},
-                    {tipo: 'Proteina', nombre: 'Pechuga de pavo'},
-                    {tipo: 'Proteina', nombre: 'Posta Rosada'},
-                    {tipo: 'Proteina', nombre: 'Soya'},
-                    {tipo: 'Proteina', nombre: 'Huevos'},
-                    {tipo: 'Grasa', nombre: 'Palta'},
-                    {tipo: 'Grasa', nombre: 'Aceite'},
-                    {tipo: 'Grasa', nombre: 'Mantequilla'},
-                    {tipo: 'Grasa', nombre: 'Semillas'},
-                    {tipo: 'Grasa', nombre: 'Manteca Animal'}];
+function inicializarLista(){
+    let lista = [
+        {
+            "nombre" : "Posta rosada",
+            "categoria": "Proteina",
+            "cantidad": 22
+        },
+        {
+            "nombre" : "Pechuga de pollo",
+            "categoria": "Proteina",
+            "cantidad": 21
+        },
+        {
+            "nombre" : "Pechuga de pavo",
+            "categoria": "Proteina",
+            "cantidad": 21
+        },
+        {
+            "nombre" : "Soja",
+            "categoria": "Proteina",
+            "cantidad": 38
+        },
+        {
+            "nombre" : "Semillas de calabaza",
+            "categoria": "Proteina",
+            "cantidad": 30
+        },
+        {
+            "nombre" : "Azúcar de mesa",
+            "categoria": "Carbohidrato",
+            "cantidad": 100
+        },
+        {
+            "nombre" : "Manjar",
+            "categoria": "Carbohidrato",
+            "cantidad": 54
+        },
+        {
+            "nombre" : "Galletas",
+            "categoria": "Carbohidrato",
+            "cantidad": 65
+        },
+        {
+            "nombre" : "Avena",
+            "categoria": "Carbohidrato",
+            "cantidad": 68
+        },
+        {
+            "nombre" : "Arroz",
+            "categoria": "Carbohidrato",
+            "cantidad": 76
+        },
+        {
+            "nombre" : "Spaghetti",
+            "categoria": "Carbohidrato",
+            "cantidad": 75
+        },
+        {
+            "nombre" : "Mantequilla",
+            "categoria": "Grasas",
+            "cantidad": 83
+        },
+        {
+            "nombre" : "Aceite",
+            "categoria": "Grasas",
+            "cantidad": 100
+        },
+        {
+            "nombre" : "Palta/Aguacate",
+            "categoria": "Grasas",
+            "cantidad": 15
+        },
+        {
+            "nombre" : "Manteca de cerdo",
+            "categoria": "Grasas",
+            "cantidad": 100
+        },
+        {
+            "nombre" : "Queso",
+            "categoria": "Grasas",
+            "cantidad": 26
+        },
+        {
+            "nombre" : "Crema de leche",
+            "categoria": "Grasas",
+            "cantidad": 37
+        },
+    ];
 
-function iniciarSesion() {
-    let nombre = prompt("Ingresar nombre de usuario");
-
-    while(nombre === undefined || nombre === null || nombre === ""){
-        alert("Debes ingresar un nombre valido");
-        nombre = prompt("Ingresar nombre de usuario");
-    }
-
-    alert(`Bienvenido ${nombre}!`);
+    localStorage.setItem("listaAlimentos", JSON.stringify(lista));
+    actualizarLista();
+    Swal.fire({
+        title: 'Lista actualizada!',
+        text: `Se ha inicializado la tabla con datos de ejemplo`,
+        icon: 'success',
+        confirmButtonText: 'Entiendo'
+    });
 }
 
-function filtrarAlimentos(){
-    let categoria = prompt("Ingresar tipo de alimento (Carbohidrato, Proteina, Grasa)");
-    while(categoria === undefined || categoria === null || categoria === ""){
-        alert("Debes ingresar una categoria valida");
-        nombre = prompt("Ingresar tipo de alimento (Carbohidrato, Proteina, Grasa)");
+function actualizarLista(){
+    listaLocalStorage = localStorage.getItem('listaAlimentos');
+    if(listaLocalStorage){
+        listaAlimentos = JSON.parse(listaLocalStorage);
+        actualizarTabla(listaAlimentos);
+    }else{
+        listaAlimentos = [];
     }
+}
 
-    let alimentosFiltrados = alimentos.filter(alimento => alimento.tipo.toUpperCase() == categoria.toUpperCase());
-    
-    let alimentosAlert = "";
+function actualizarTabla(lista){
+    const tabla = document.getElementById("tablaAlimentos");
+    const tbody = tabla.getElementsByTagName("tbody")[0];
+    tbody.innerHTML = "";
 
-    alimentosFiltrados.forEach(alimento => {
-        alimentosAlert = `${alimentosAlert} - ${alimento.nombre}`;
+    lista.forEach(function (alimento, indice) {
+        const fila = tbody.insertRow();
+        let celda1 = fila.insertCell(0);
+        let celda2 = fila.insertCell(1);
+        let celda3 = fila.insertCell(2);
+        let celda4 = fila.insertCell(3);
+
+        var botonEliminar = document.createElement("button");
+        botonEliminar.classList.add("btn");
+        botonEliminar.classList.add("btn-danger");
+        botonEliminar.textContent = "Eliminar";
+        botonEliminar.addEventListener("click", function () {
+            eliminarElemento(indice-1);
+        });
+
+        celda1.innerHTML = alimento.nombre;
+        celda2.innerHTML = alimento.categoria;
+        celda3.innerHTML = alimento.cantidad;
+        celda4.appendChild(botonEliminar);
+        indice++;
+    });
+}
+
+function eliminarElemento(indice) {
+    listaAlimentos.splice(indice, 1);
+    localStorage.setItem("listaAlimentos", JSON.stringify(listaAlimentos));
+    actualizarTabla(listaAlimentos);
+    Swal.fire({
+        title: 'Lista actualizada!',
+        text: `Se ha eliminado el alimento correctamente`,
+        icon: 'success',
+        confirmButtonText: 'Entiendo'
+    });
+}
+
+function filtrarTabla(filtro) {
+    var filtroLowerCase = filtro.toLowerCase();
+    var listaFiltrada = listaAlimentos.filter(function (alimento) {
+        return alimento.nombre.toLowerCase().includes(filtroLowerCase) || alimento.categoria.toLowerCase().includes(filtroLowerCase);
     });
 
-    alert(`Alimentos encontrados: ${alimentosAlert}`);
+    actualizarTabla(listaFiltrada);
 }
+
+function crearAlimento(){
+    let nombre = document.getElementById("nombreNuevo");
+    let categoria = document.getElementById("categoriaNuevo");
+    let cantidad = document.getElementById("cantidadNuevo");
+
+    let alimentoNuevo = {
+        "nombre" : nombre.value,
+        "categoria": categoria.value,
+        "cantidad": cantidad.value
+    }
+
+    listaLocalStorage = localStorage.getItem('listaAlimentos');
+
+    listaAlimentos = JSON.parse(listaLocalStorage);
+    listaAlimentos.push(alimentoNuevo);
+    localStorage.setItem("listaAlimentos", JSON.stringify(listaAlimentos));
+    actualizarTabla(listaAlimentos);
+
+    Swal.fire({
+        title: 'Lista actualizada!',
+        text: `Se ha creado el alimento correctamente`,
+        icon: 'success',
+        confirmButtonText: 'Entiendo'
+    });
+}
+
